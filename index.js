c54f26ab221f4889f9d13e74877e283e7d62ca89
Felix Maiko <maikofelix89@gmail.com>
2021-08-26 15:42:09 +0300

    Added divide function (#2)

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function sum(a,b){
     return (a+b);
 }
+function divide(a,b){
+  return (a/b);
+}
 
 const total = sum(2,3);
 console.log(total);
+const divideValue = divide(10,2);
+console.log('Divide', divideValue);
