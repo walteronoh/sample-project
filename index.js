12427fa37ead0c04c00d5acedde19038d844feef
walter <walterkiprono81@gmail.com>
2021-08-26 15:44:13 +0300

    added subtract function

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ const total = sum(2,3);
 console.log(total);
 const divideValue = divide(10,2);
 console.log('Divide', divideValue);
+
+function subtract(a,b){
+    return (a-b);
+}
+
+const subtract_total = subtract(6,2);
+console.log(subtract_total);
